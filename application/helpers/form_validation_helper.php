4f92ffda2cc95bc7ac0a2bd0dfb3b09b8cfae60b
lixm <1027093208@qq.com>
2020-08-16 16:54:38 +0800

    update

--- application/helpers/form_validation_helper.php
+++ application/helpers/form_validation_helper.php
@@ -29,7 +29,7 @@ if (!function_exists('is_mobile')) {
             return false;
         }
         //return preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $mobile) ? true : false;
-        return preg_match('#^1[\d]{}$#', $mobile) ? true : false;
+        return preg_match('#^1[\d]{10}$#', $mobile) ? true : false;
     }
 }
 
