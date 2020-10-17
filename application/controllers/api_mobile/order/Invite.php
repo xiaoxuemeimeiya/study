80ab9870000d5c356f05e24416fadd7a1e733703
lixm <1027093208@qq.com>
2020-10-17 16:17:36 +0800

    update

--- application/controllers/api_mobile/order/Invite.php
+++ application/controllers/api_mobile/order/Invite.php
@@ -76,7 +76,7 @@ class Invite extends MY_Controller
        $where_data['where']['o.m_id'] = $m_id;
 
        //搜索条件end
-       $where_data['select'] = 'o.id,o.good_id,o.order_no,o.payment_status,o.status,convert(o.order_price/10000,decimal(10,2)) as order_price,convert(o.sku_price_real/10000,decimal(10,2)) as sku_price_real,o.addtime,o.paytime,m.nickname,k.name';
+       $where_data['select'] = 'o.id,o.good_id,o.order_no,o.payment_status,o.status,convert(o.order_price/10000,decimal(10,2)) as order_price,convert(o.sku_price_real/10000,decimal(10,2)) as sku_price_real,o.addtime,o.paytime,m.nickname,k.name,k.time';
        $where_data['join']   = array(
            array('user as m', 'o.m_id=m.id'),
            array('goods as k', 'o.good_id=k.id'),
