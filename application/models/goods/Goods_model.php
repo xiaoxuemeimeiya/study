a1c991c3e0a9120715b5966f6011603e81cd0a05
lixm <1027093208@qq.com>
2020-10-17 15:29:07 +0800

    update

--- application/models/goods/Goods_model.php
+++ application/models/goods/Goods_model.php
@@ -22,7 +22,7 @@ class Goods_model extends CI_Model
     {
         $id = (int)$id;
         if (!empty($id)) {
-            $goods_data = $this->loop_model->get_where('goods', array('id'=>$id),'id,vedio,name,sub_desc,sale,visit,market_price,sell_price');
+            $goods_data = $this->loop_model->get_where('goods', array('id'=>$id),'id,vedio,name,sub_desc,sale,visit,market_price,sell_price,time');
             if (empty($goods_data)) return false;//msg('商品不存在');
             if ($goods_data['status'] != 0) return false; //msg('商品已下架');
             $goods_data['market_price'] = format_price($goods_data['market_price']);
